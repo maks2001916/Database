0a76cea53d2512fd140641a1d47f3b04d6adc970
maks2001916 <mamonov.maksim.20003@gmail.com>
2025-01-23 13:25:54 +0500

    Элементы разметки main класса связаны с соответствующими объектами

--- app/src/main/java/com/example/database/MainActivity.kt
+++ app/src/main/java/com/example/database/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.example.database
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var titleTV: TextView
+    private lateinit var startBTN: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        init()
+
+
+
+    }
+
+    private fun init() {
+        titleTV = findViewById(R.id.appNameTV)
+        startBTN = findViewById(R.id.openDbBTN)
     }
 }
